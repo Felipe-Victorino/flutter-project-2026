3bc47f264e1426629001493c367072175c9bce72
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-04-13 19:28:57 -0300

    feat(main.dart): the app is now a state widget, allows fo rthe dynamic change of theme between light and dark

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,50 @@ void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
-  static const Color seedColor = Colors.pink;
+  @override
+  _AppState createState() => _AppState();
+
+  static _AppState of(BuildContext context) =>
+      context.findRootAncestorStateOfType<_AppState>()!;
+}
+
+class _AppState extends State<App> {
+  ThemeMode _theme = ThemeMode.system;
+
+  void changeTheme(ThemeMode theme) {
+    setState(() {
+      _theme = theme;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: Locale('pt-BR'),
       title: 'Test App',
       debugShowCheckedModeBanner: false,
 
-      theme: ThemeData(
-        brightness: .light,
-
-        useMaterial3: true,
-        colorScheme: .fromSeed(seedColor: seedColor, brightness: .light),
-      ),
-      darkTheme: ThemeData(
-        brightness: .dark,
-        useMaterial3: true,
-        colorScheme: .fromSeed(seedColor: seedColor, brightness: .dark),
-      ),
-      themeMode: ThemeMode.system,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: _theme,
 
       home: const Home(),
     );
   }
 }
+
+const Color seedColor = Colors.red;
+final ThemeData darkTheme = ThemeData(
+  brightness: .dark,
+  useMaterial3: true,
+  colorScheme: .fromSeed(seedColor: seedColor, brightness: .dark),
+);
+
+final ThemeData lightTheme = ThemeData(
+  brightness: .light,
+  useMaterial3: true,
+  colorScheme: .fromSeed(seedColor: seedColor, brightness: .light),
+);
